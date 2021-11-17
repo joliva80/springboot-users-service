fe94389df09be8b096c3aa3ca93d8d6444daff11
Joan Oliva <joanoj@gmail.com>
2021-11-17 10:32:42 +0100

    Added jwt token authentication

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,12 +1,14 @@
 
 INSERT INTO users (username, password, enabled, name, email) VALUES ('john', '$2a$04$jJ.cIt03C4H2hmzgAju5ce/FtmYE2B1fgBaLGxVusFSGgZxx5RR/e', 1, 'John', 'john@gmail.com');
 INSERT INTO users (username, password, enabled, name, email) VALUES ('emily', '$2a$04$jJ.cIt03C4H2hmzgAju5ce/FtmYE2B1fgBaLGxVusFSGgZxx5RR/e', 1, 'Emilly', 'emily@gmail.com');
-INSERT INTO users (username, password, enabled, name, email) VALUES ('matt', '1234', 1, 'Matt', 'matt@gmail.com');
-INSERT INTO users (username, password, enabled, name, email) VALUES ('cindy', '1234', 1, 'Cindy', 'cindy@gmail.com');
+INSERT INTO users (username, password, enabled, name, email) VALUES ('matt', '$2a$04$hiVuc40MqkW1W5kD.WO9j.2h.IcNcMcTl//RkmNgFniDfrGdTSymm', 1, 'Matt', 'matt@gmail.com');
+INSERT INTO users (username, password, enabled, name, email) VALUES ('cindy', '$2a$04$hiVuc40MqkW1W5kD.WO9j.2h.IcNcMcTl//RkmNgFniDfrGdTSymm', 1, 'Cindy', 'cindy@gmail.com');
 
-INSERT INTO roles (name) VALUES ('ROLE_USER');
-INSERT INTO roles (name) VALUES ('ROLE_ADMIN');
+INSERT INTO roles (name) VALUES ('ROLE_USER'); -- 1
+INSERT INTO roles (name) VALUES ('ROLE_ADMIN'); -- 2
 
 INSERT INTO users_roles (user_id, roles_id) VALUES (1,1);
 INSERT INTO users_roles (user_id, roles_id) VALUES (2,2);
 INSERT INTO users_roles (user_id, roles_id) VALUES (2,1);
+INSERT INTO users_roles (user_id, roles_id) VALUES (3,1);
+INSERT INTO users_roles (user_id, roles_id) VALUES (4,2);
